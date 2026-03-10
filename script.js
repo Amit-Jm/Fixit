8065089f80c3f4f349779cf0cec8d3b0f31b80e3
Amit-Jm <amitjames795@gmail.com>
2026-03-10 13:15:02 +0530

    script.js
    update

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-// Scroll to repair section when Book Repair button clicked
+// Scroll to repair section
 
-document.getElementById("bookBtn").onclick = function(){
+document.getElementById("bookBtn").onclick=function(){
 
 document.getElementById("repair").scrollIntoView();
 
 };
 
 
-
-
-// Form submit event
+// Form Submit -> WhatsApp
 
 document.getElementById("repairForm").addEventListener("submit",function(e){
 
 e.preventDefault();
 
-document.getElementById("successMsg").innerText =
-"Repair request sent successfully. We will contact you soon.";
+let name=document.getElementById("name").value;
+let phone=document.getElementById("phone").value;
+let address=document.getElementById("address").value;
+let brand=document.getElementById("brand").value;
+let problem=document.getElementById("problem").value;
+let details=document.getElementById("details").value;
+
+let message=`New Mobile Repair Booking
+
+Name: ${name}
+Phone: ${phone}
+Address: ${address}
+
+Brand: ${brand}
+Problem: ${problem}
+
+Details: ${details}`;
+
+let url="https://wa.me/918603399409?text="+encodeURIComponent(message);
+
+window.open(url,"_blank");
 
 });
